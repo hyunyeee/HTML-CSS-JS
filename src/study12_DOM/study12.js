f65e5a343682c1fae07a22559bdef0a74d97b38d
hyunyeee <5004ohy@naver.com>
2022-11-11 23:59:01 +0900

    DOM

--- src/study12_DOM/study12.js
+++ src/study12_DOM/study12.js
@@ -1,11 +1,65 @@
-let pics = document.getElementsByClassName("pic"); //pic인 요소를 모두 가져옴
-let lightbox = document.getElementById("lightBox"); //라이트 박스 요소를 가져옴
-let lightboxImage = document.getElementById("lightboxImage"); //라이트박스 이미지를 가져옴
 
 
-/*pics 배열의 i번째 요소를 클릭할 경우 showLightbox 함수 실행*/
-for(i = 0; i<pics.length; i++){
-    pics[i].addEventListener("click", showLightbox);
+
+
+function newResister() {
+    /*요소 노드 추가하기*/
+
+    document.querySelector('#itemList').style.display = "block";
+
+    let newItem = document.createElement('li'); //요소 노드 추가
+
+    let item = document.querySelector('#item'); //폼의 텍스트 필드
+
+    let newText = document.createTextNode(item.value); //텍스트 필드의 값을 텍스트 노드로 만들기
+
+    newItem.appendChild(newText); //텍스트 노드를 요소 노드의 자식 노드로 추가
+
+    let itemList = document.querySelector("#itemList"); //웹 문서에서 부모 노드 가져오기
+
+    /*
+    itemList.appendChild(newItem); //새로 만든 요소 노드를 부모 노드에 추가
+
+    appendChild() 메소드는 새로운 노드를 부모 노드의 맨 끝에 추가
+    : 마지막에 입력한 값은 목록 맨 아래에 표시
+
+    마지막 자식 노드를 맨 앞의 자식 노드 앞에 추가하면
+    마지막에 입력한 값을 맨 위에 나타낼 수 있음
+    */
+
+    itemList.insertBefore(newItem, itemList.childNodes[0]);
+
+    item.value = "";
+
+
+    /*클릭하면 노드 삭제하기*/
+    let items = document.querySelectorAll("li"); //모든 항목 가져오기
+
+    for (let i=0; i<items.length; i++) {
+        items[i].addEventListener("click", function () { //i번째 요소 클릭했을 때 함수 실행
+            if(this.parentNode)
+                this.parentNode.removeChild(this);
+
+            if(items.length===0){
+                document.querySelector('#itemList').style.display = "none";
+            }
+
+
+        })
+    }
+
+
+
+    /*
+
+    만약에 ul에 itemList가 비어있으면
+    *
+    * display none
+    *
+    * */
+
+
+
 }
 
 
@@ -14,9 +68,10 @@ for(i = 0; i<pics.length; i++){
 
 
 
-/*클릭한 이미지의 큰 이미지 파일 경로를 가져와서 라이트 박스에 있는 이미지 파일 경로 변경*/
-function showLightbox() {
-    let bigLocation = this.getAttribute("src"); //섬네일 이미지 속성값 가져옴
-    lightboxImage.setAttribute("src", bigLocation);
-    lightbox.style.display = "block";
-}
+
+
+
+
+
+
+
---
+++ src/unresolved_DOM/study12.js
@@ -0,0 +1,28 @@
+let pics = document.getElementsByClassName("pic"); //pic인 요소를 모두 가져옴
+let lightbox = document.getElementById("lightBox"); //라이트 박스 요소를 가져옴
+let lightboxImage = document.getElementById("lightboxImage"); //라이트박스 이미지를 가져옴
+
+document.write(pics.length)
+
+/*pics 배열의 i번째 요소를 클릭할 경우 showLightbox 함수 실행*/
+for(let i = 0; i<pics.length; i++){
+
+
+    pics[i].addEventListener("click", showLightbox);
+}
+
+
+
+
+
+
+
+/*클릭한 이미지의 큰 이미지 파일 경로를 가져와서 라이트 박스에 있는 이미지 파일 경로 변경*/
+function showLightbox() {
+    let bigLocation = this.getAttribute("src"); //섬네일 이미지 속성값 가져옴
+
+
+
+    lightboxImage.setAttribute("src", bigLocation);
+    lightbox.style.display = "block";
+}
